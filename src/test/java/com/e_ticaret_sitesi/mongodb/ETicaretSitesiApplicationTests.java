14542eea610d049a99b2947cc3620240552c2334
Sadullah Özden <sdllhozden@gmail.com>
2025-11-12 10:48:30 +0300

    güncel

---
+++ src/test/java/com/e_ticaret_sitesi/mongodb/ETicaretSitesiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e_ticaret_sitesi.mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ETicaretSitesiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
